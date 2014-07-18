6c4572b20491f1b13264982d74f9d44a4fec2738
Doichin Denchev <doichindenchev@Doichins-MacBook-Air.local>
2014-07-18 19:54:07 -0400

    basic flow player speed api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,39 @@
 console.log('\'Allo \'Allo!');
+
+
+appState = {
+  playSpeed: 1
+}
+
+
+flowplayer(function (api, root) {
+ 
+  api.bind("load", function () {
+ 
+  // do something when a new video is about to be loaded
+ 
+  }).bind("ready", function () {
+ 
+    console.log(api);
+    api.play();
+
+    $(document).keypress(function (eh){ 
+
+      if (eh.keyCode === 38) {
+        appState.playSpeed += 0.1;
+        api.speed(appState.playSpeed);
+      } else if (eh.keyCode === 40) {
+        appState.playSpeed -= 0.1;
+        api.speed(appState.playSpeed);
+      }
+
+      console.log(eh.keyCode);
+    });
+
+ 
+  });
+ 
+});
+
+
+
