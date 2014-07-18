465bf524e7d5c3eff1e2326f961411ad697da4f6
google.marielbartolome <google.marielbartolome@gmail.com>
2014-07-18 19:38:23 -0400

    leap connect and x and y direction stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  var ctl = new Leap.Controller({enableGestures: true});
+
+  ctl.on( 'connect' , function(){
+    console.log( 'Successfully connected.' );
+  });
+
+  ctl.on( 'disconnect' , function(){
+    console.log( 'LeapMotion disconnected.' );
+  });
+
+  var swiper = ctl.gesture('swipe');
+  var xTolerance = 50;
+  var yTolerance = 30;
+
+  swiper.update(function(g) {
+    if (Math.abs(g.translation()[0]) > xTolerance || Math.abs(g.translation()[1]) > yTolerance) {
+      var xDir = Math.abs(g.translation()[0]) > xTolerance ? (g.translation()[0] > 0 ? -1 : 1) : 0;
+      var yDir = Math.abs(g.translation()[1]) > yTolerance ? (g.translation()[1] < 0 ? -1 : 1) : 0;
+      console.log("x direction: " + xDir + " y direction: " + yDir);
+    }
+  });
+
+  ctl.connect();
+})
