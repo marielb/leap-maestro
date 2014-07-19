528aabdca5cbb1066f2bd654cc922f15da5b87c4
Doichin Denchev <doichin.denchev@gmail.com>
2014-07-19 12:10:29 -0400

    flow player back in stock

--- app/scripts/leap_conductor.js
+++ app/scripts/leap_conductor.js
@@ -1,22 +1,6 @@
 leapConductor = {
 
   initialize: function() {
-    this.initFlowPlayer();
     leapController.initialize();
-  },
-
-  initFlowPlayer: function() {
-    flowplayer(function (api, root) {
-      flowPlayer.api = api;
-
-      api.bind('load', function () {
-        flowPlayer.load(api);
-        console.log('load');
-
-      }).bind('ready', function () {
-        flowPlayer.ready(api);
-        console.log('reload');
-      });
-    });
   }
 };
