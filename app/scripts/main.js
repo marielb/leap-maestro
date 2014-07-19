028355b5ad459132c93da9018ac1ed24b2da66e7
google.marielbartolome <google.marielbartolome@gmail.com>
2014-07-18 20:16:33 -0400

    beats per minute stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,14 +12,53 @@ $(document).ready(function() {
   var swiper = ctl.gesture('swipe');
   var xTolerance = 50;
   var yTolerance = 30;
+  var swipeLeft = false;
+  var current = Date.now();
+
+  var timez = [];
+  var average = null;
 
   swiper.update(function(g) {
     if (Math.abs(g.translation()[0]) > xTolerance || Math.abs(g.translation()[1]) > yTolerance) {
       var xDir = Math.abs(g.translation()[0]) > xTolerance ? (g.translation()[0] > 0 ? -1 : 1) : 0;
       var yDir = Math.abs(g.translation()[1]) > yTolerance ? (g.translation()[1] < 0 ? -1 : 1) : 0;
-      console.log("x direction: " + xDir + " y direction: " + yDir);
+      // console.log("x direction: " + xDir + " y direction: " + yDir);
+
+      if (xDir == -1 && swipeLeft == false) {
+        swipeLeft = true;
+
+        current = resetTime(current);
+      } else if (xDir == 1 && swipeLeft == true) {
+        swipeLeft = false;
+
+        current = resetTime(current);
+      }
     }
   });
 
   ctl.connect();
+
+  function resetTime(current) {
+    diff = (Date.now() - current) / 1000;
+    console.log(diff);
+
+    if (timez.length < 9) {
+        timez.push(diff);
+    } else if (timez.length == 9) {
+        var average = getAverage(timez);
+        console.log("average: " + average);
+    }
+
+    return Date.now();
+  }
+
+  function getAverage(arr) {
+    var sum = 0;
+    for (var x = 1; x < timez.length; x++) {
+        sum = sum + timez[x];
+    }
+
+    return sum / 8;
+  }
+
 })
